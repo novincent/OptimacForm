b9cb2c18dde6a5765cfb2b8527e25cdfcaae326a
novincent <noe.vincent@ens-rennes.fr>
2026-02-10 17:53:15 +0100

    small updates

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ This repository stores the source code used to produce the survey presented in :
 - `src` stores the source code used to produce the form but also to compute the data
 - `dataAnalysis` stores the python files used to filter/visualize/format the data output
 - `simulation` stores the python files used to run optimac on the data output from the survey
+
+The original source code is hosted at https://github.com/novincent/OptimacForm
---
+++ simulation/mathplayground.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+
+
+
+a = {'weekly': {-3: 32, -2: 18, -1: 13, 0: 1, 1: 11, 2: 2, 3: 4}, 'monthly': {-3: 3, -2: 3, -1: 12, 0: 2, 1: 21, 2: 24, 3: 16}, 'as_usual': {-3: 1, -2: 5, -1: 0, 0: 8, 1: 15, 2: 29, 3: 23}}
+b = a.values()
+
+c = [list(x.values()) for x in b]
+
+c[0] = c[0][::-1]
+
+arr = np.array(c)
+
+s = arr.sum(axis = 0)
+ds = [sum(s[:i+1]) for i in range(len(s))]
+ds/=ds[-1]
+print(ds)
+ms = arr.mean(axis = 0)
+
+m = ms/ms.sum()
+plt.bar(np.linspace(-3,3,7),m)
+plt.xlabel("Motivation")
+plt.ylabel("Probability of expression")
+plt.title("Approximated Trend Motivation Distribution in the Mowing Scenario (Mass function)")
+plt.show()
+
+plt.bar(np.linspace(-3,3,7),ds)
+plt.xlabel("Motivation")
+plt.ylabel("Probability of expression")
+plt.title("Approximated Trend Motivation Distribution in the Mowing Scenario")
+plt.show()
